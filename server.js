da396e86961b40190826ac6ebbf1b9a3635a4e4e
AliQannan <126164190+AliQannan@users.noreply.github.com>
2025-08-31 16:20:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ const PORT = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World! Welcome to my Express server.');
 });
+app.get("/aliexpress/callback", async (req, res) => {
+  const code = req.query.code; // هذا الـ Authorization Code
+  const state = req.query.state; // اختياري، للتحقق من الأمان
+
+  if (!code) {
+    return res.status(400).send("No authorization code received!");
+  }
+
 
 // 5. Start the server
 app.listen(PORT, () => {
